8e7eba33028088b63a07db788409a089885ee35b
Moji <mojtaba.raznahan@gmail.com>
2022-03-14 16:18:21 +0330

    Initial Commit

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+
+constructor(){
+    console.log("Enjoying Buildspace's web3 tutorials!");
+}
+
+}
