919b5fbdbeff8a9c4bf3fce07f75a3e3ab34edc2
Moji <mojtaba.raznahan@gmail.com>
2022-03-15 10:22:25 +0330

    Change NFT storage to Base64String

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -18,8 +18,8 @@ contract MyEpicNFT is ERC721URIStorage {
 
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/J72Z");
-        
+        _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiU2NlbmljQXdlc29tZVJvdXRlIiwKICAgICJkZXNjcmlwdGlvbiI6ICJBbiBORlQgZnJvbSBNb2ppLCB3aXRoIGxvdmUiLAogICAgImltYWdlIjogImRhdGE6aW1hZ2Uvc3ZnK3htbDtiYXNlNjQsUEhOMlp5QjRiV3h1Y3owaWFIUjBjRG92TDNkM2R5NTNNeTV2Y21jdk1qQXdNQzl6ZG1jaUlIQnlaWE5sY25abFFYTndaV04wVW1GMGFXODlJbmhOYVc1WlRXbHVJRzFsWlhRaUlIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJajRLSUNBZ0lEeHpkSGxzWlQ0dVltRnpaU0I3SUdacGJHdzZJSGRvYVhSbE95Qm1iMjUwTFdaaGJXbHNlVG9nYzJWeWFXWTdJR1p2Ym5RdGMybDZaVG9nTVRSd2VEc2dmVHd2YzNSNWJHVStDaUFnSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaUJtYVd4c1BTSmliR0ZqYXlJZ0x6NEtJQ0FnSUR4MFpYaDBJSGc5SWpVd0pTSWdlVDBpTlRBbElpQmpiR0Z6Y3owaVltRnpaU0lnWkc5dGFXNWhiblF0WW1GelpXeHBibVU5SW0xcFpHUnNaU0lnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJK1UyTmxibWxqUVhkbGMyOXRaVkp2ZFhSbFBDOTBaWGgwUGdvOEwzTjJaejQ9IiIKfQo=");
+
         console.log("An NFT w/ ID %s has been minted to %s",newItemId,msg.sender);
         _tokenIds.increment();
     }
