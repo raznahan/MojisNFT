05c5fad91205f83c81e36ef027daf98f910ab1bd
Moji <mojtaba.raznahan@gmail.com>
2022-03-19 14:42:07 +0330

    Add loading icon while minting

--- website/src/App.js
+++ website/src/App.js
@@ -17,7 +17,8 @@ const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
   const [maxMint, setMaxMint] = useState("0");
   const [minted, setMinted] = useState("0");
-  const [mintButtonDisabled,setMintButtonDisabled] =useState(false);
+  const [mintButtonDisabled, setMintButtonDisabled] = useState(false);
+  const [loaderClass, setLoaderClass] = useState("");
 
   const checkIfWalletIsConnected = async () => {
 
@@ -95,7 +96,7 @@ const App = () => {
       const totalMinted = await MyEpicNFTContract.getTotalNFTsMinted();
       console.log('totalMinted:' + totalMinted);
       const maxMint = await MyEpicNFTContract.maxMint();
-      if(totalMinted >= maxMint)
+      if (totalMinted >= maxMint)
         setMintButtonDisabled(true);
       console.log('maxMint: ' + maxMint);
       setMinted(totalMinted.toString());
@@ -108,32 +109,24 @@ const App = () => {
   // Setup our listener.
   const setupEventListener = async () => {
     // Most of this looks the same as our function askContractToMintNft
-    try {
-      const { ethereum } = window;
+    const MyEpicNFTContract = getContract();
+    if (MyEpicNFTContract) {
+      // THIS IS THE MAGIC SAUCE.
+      // This will essentially "capture" our event when our contract throws it.
+      // If you're familiar with webhooks, it's very similar to that!
+      MyEpicNFTContract.on("NewEpicNFTMinted", (from, tokenId) => {
+        console.log(from, tokenId.toNumber())
+        alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+      });
+     
+      console.log("Setup event listener!");
 
-      if (ethereum) {
-        // Same stuff again
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
-
-        // THIS IS THE MAGIC SAUCE.
-        // This will essentially "capture" our event when our contract throws it.
-        // If you're familiar with webhooks, it's very similar to that!
-        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
-          console.log(from, tokenId.toNumber())
-          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
-        });
-
-        console.log("Setup event listener!")
-
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
-    } catch (error) {
-      console.log(error)
     }
-  }
+    else {
+      console.log('error in contract creation');
+    }
+  };
+
   const getContract = () => {
     const { ethereum } = window;
 
@@ -141,6 +134,7 @@ const App = () => {
       const provider = new ethers.providers.Web3Provider(ethereum);
       const signer = provider.getSigner();
       const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
       return connectedContract;
     }
     else {
@@ -152,17 +146,20 @@ const App = () => {
     try {
       const MyEpicNFTContract = getContract();
       if (MyEpicNFTContract) {
+        setLoaderClass("fa fa-spinner fa-spin");
         console.log("Going to pop wallet now to pay gas...")
         let nftTxn = await MyEpicNFTContract.makeAnEpicNFT();
 
         console.log("Mining...please wait.")
         await nftTxn.wait();
+        setLoaderClass("");
         console.log(nftTxn);
         console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
       } else {
         window.alert("Ethereum object doesn't exist!")
       }
     } catch (error) {
+      setLoaderClass("");
       console.log(error)
     }
   }
@@ -178,11 +175,12 @@ const App = () => {
     </button>
   );
 
+
   const renderMintUI = () => (
     <div>
       <p className="sub-text">{minted}/{maxMint} minted so far!</p>
-      <button id="mintBtn" onClick={askContractToMintNft} disabled={mintButtonDisabled} className="cta-button connect-wallet-button">
-        Mint NFT
+      <button onClick={askContractToMintNft} disabled={mintButtonDisabled} className="connect-wallet-button">
+        <i className={loaderClass}></i>Mint NFT
       </button>
     </div>
   )
